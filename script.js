20ca78b5c7427e76d364d7f477b627958092c4e6
Ian Agustin <ian.agustin071729@gmail.com>
2022-01-25 07:35:58 +0800

    Added random color feature

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
+'use strict';
+
 const container = document.querySelector('.container');
 const colorInput = document.querySelector('#color-picker');
 const colorInputHex = document.querySelector('#cp-value');
 const sliderValue = document.querySelector('#slider');
 const sliderText = document.querySelector('.slider-text');
-const clearBtn = document.querySelector('.clear-btn');
+const clearBtn = document.querySelector('.clear');
+const randBtn = document.querySelector('.random');
 
 let panels, panel;
+let isRandomMode = false;
 
-const MakeGrid = function(size) {
-    sizeSqrd = Math.pow(size, 2);
+
+const MakeGrid = (size) => {
+    const sizeSqrd = Math.pow(size, 2);
 
     for (let i = 0; i < sizeSqrd; i++) {
         panel = document.createElement('div');
         panel.classList.add('panel');
         container.append(panel);
         container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        console.log("panel added");
     }
 
     panels = document.querySelectorAll('.panel');
     panels.forEach((panel) => {
         panel.addEventListener('mouseenter', function(e) {
-            e.target.style.background = colorInput.value;
+            if (isRandomMode === false) e.target.style.background = colorInput.value;
+            else if (isRandomMode === true) e.target.style.background = GenerateColor();
         });
     });
+}
+
+const GenerateColor = () => {
+    let randColor = Math.floor(Math.random()*16777215).toString(16);
+    return `#${randColor}`;
 }
 
 MakeGrid(sliderValue.value);
@@ -36,6 +46,19 @@ clearBtn.addEventListener('click', () => {
     panels.forEach((panel) => {
         panel.style.background = 'white';
     });
+});
+
+randBtn.addEventListener('click', () => {
+    if (isRandomMode === false) {
+        randBtn.style.background = '#EDC610';
+        randBtn.style.border = '2px solid #EDC610';
+        isRandomMode = true;
+    }
+    else if (isRandomMode === true) {
+        randBtn.style.background = 'transparent';
+        randBtn.style.border = '2px solid #202020';
+        isRandomMode = false;
+    }
 });
 
 sliderValue.addEventListener('input', () => {
