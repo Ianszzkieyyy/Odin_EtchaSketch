1d9d2ac9abeb0331ccfe83e4114411d66220357c
Ian Agustin <ian.agustin071729@gmail.com>
2022-01-24 21:52:18 +0800

    Completed sketch features(for now)

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const container = document.querySelector('.container');
 const colorInput = document.querySelector('#color-picker');
 const colorInputHex = document.querySelector('#cp-value');
+const sliderValue = document.querySelector('#slider');
+const sliderText = document.querySelector('.slider-text');
+const clearBtn = document.querySelector('.clear-btn');
 
-let gridCount = 50; // TEMPORARY
-let panels;
+let panels, panel;
 
-const makeGrid = function(size) {
+const MakeGrid = function(size) {
     sizeSqrd = Math.pow(size, 2);
 
     for (let i = 0; i < sizeSqrd; i++) {
-        const panel = document.createElement('div');
+        panel = document.createElement('div');
         panel.classList.add('panel');
         container.append(panel);
         container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -17,17 +19,28 @@ const makeGrid = function(size) {
     }
 
     panels = document.querySelectorAll('.panel');
+    panels.forEach((panel) => {
+        panel.addEventListener('mouseenter', function(e) {
+            e.target.style.background = colorInput.value;
+        });
+    });
 }
-makeGrid(gridCount);
 
-colorInput.addEventListener('input', (e) => {
+MakeGrid(sliderValue.value);
+
+colorInput.addEventListener('input', () => {
     colorInputHex.textContent = colorInput.value
-    return colorInput.value;
 });
 
-panels.forEach((panel) => {
-    panel.addEventListener('mouseenter', function(e) {
-        e.target.style.background = colorInput.value;
+clearBtn.addEventListener('click', () => {
+    panels.forEach((panel) => {
+        panel.style.background = 'white';
     });
+});
+
+sliderValue.addEventListener('input', () => {
+    document.querySelectorAll('.panel').forEach(e => e.remove());
+    sliderText.textContent = `${sliderValue.value}x${sliderValue.value}`;
+    MakeGrid(sliderValue.value);
 });
 
